8e4344d3ceb6fd2254bfab12327ae0e9d5738bec
Rainer Simon <rainer@rainersimon.io>
2025-07-02 17:39:33 +0200

    Work in progress

---
+++ src/_index/_components/AnimatedParagraph.tsx
@@ -0,0 +1,69 @@
+import { AnimatePresence, motion } from 'motion/react';
+import type { ReactElement } from 'react';
+
+interface AnimatedParagraphProps {
+
+  html: string[];
+
+}
+
+const PARAGRAPH_VARIANTS = {
+  hidden: { 
+    filter: 'blur(8px)',
+  },
+  visible: {
+    filter: 'blur(0px)',
+    transition: {
+      duration: 3,
+      ease: 'easeIn',
+      staggerChildren: 0.15
+    }
+  },
+  exit: {
+    filter: 'blur(8px)',
+    opacity: 0,
+    transition: {
+      duration: 2,
+      ease: 'easeIn'
+    }
+  }
+};
+
+const TOKEN_VARIANTS = {
+  hidden: { opacity: 0, scale: 0.97, y: 8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 1.75,
+      ease: 'easeIn'
+    }
+  }
+};
+
+export const AnimatedParagraph = (props: AnimatedParagraphProps) => {
+  
+  return (
+    <AnimatePresence>
+      <motion.p 
+        className="motion-safe:transition-transform" 
+        variants={PARAGRAPH_VARIANTS}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{
+          amount: 0.5
+        }}>
+        {props.html.map((html, idx) => (
+          <motion.span
+            className="inline-block origin-center"
+            key={`token-${idx}`}
+            variants={TOKEN_VARIANTS}
+            dangerouslySetInnerHTML={{ __html: `${html}\u00A0`}}>
+          </motion.span>
+        ))}
+      </motion.p>
+    </AnimatePresence>
+  )
+
+}
