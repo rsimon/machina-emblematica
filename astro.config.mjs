a7e855264650f2579807b5c37c600f491af98d89
Rainer Simon <rainer@rainersimon.io>
2025-10-24 08:59:53 +0200

    Netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  integrations: [react()],
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
     server: {
       open: 'test/index.html',
     }
-  },
-  integrations: [react()]
+  }
 });
